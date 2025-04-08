a97d543a34e974465820a3d8383652a73de3a96c
mariocos <alpacasrcool7@gmail.com>
2025-04-08 14:31:51 +0100

    small formating on file

--- learing_py/ex00.py
+++ learing_py/ex00.py
@@ -1,13 +1,9 @@
 import requests
 
-
-
-
 ft_list = ["Hello", "tata!"]
 ft_tuple = ("Hello", "toto!")
 ft_set = {"Hello", "tutu!"}
 ft_dict = {"Hello" : "titi!"}
-
 
 response = requests.get('https://ipinfo.io/json')
 data = response.json()
@@ -19,8 +15,6 @@ ft_list[1] = "World!"
 ft_tuple = (ft_tuple[0], country)
 ft_set = {"Hello", city}
 ft_dict["Hello"] = "42" + city
-
-
 
 print(ft_list)
 print(ft_tuple)
